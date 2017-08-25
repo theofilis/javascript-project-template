12e828ffcc9d3d6f1933b12a68d1be940ad421ea
George Theofilis <theofilis.g@gmail.com>
2017-08-25 15:35:23 +0300

    Automatically install npm dependencies

---
+++ src/App.ts
@@ -0,0 +1,3 @@
+const a = () => {
+  const temp = 0;
+};
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,37 @@
-import * as webpack from 'webpack';
 import * as path from 'path';
+import * as webpack from 'webpack';
+
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
 declare var __dirname: any;
 
 const config: webpack.Configuration = {
-  entry: './src/foo.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'foo.bundle.js'
-  },
+  target: 'node',
+  context: `${__dirname}/src/`,
+  entry: './App.ts',
   module: {
     rules: [{
-      test: /\.ts(x?)$/,
       exclude: /node_modules/,
-      loader: 'awesome-typescript-loader'
+      loader: 'awesome-typescript-loader',
+      test: /\.ts(x?)$/
     }]
-  }
+  },
+  output: {
+    filename: 'App.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new NpmInstallPlugin({
+      // Use --save or --save-dev
+      dev: false,
+      // Install missing peerDependencies
+      peerDependencies: true,
+      // Reduce amount of console logging
+      quiet: false,
+      // npm command used inside company, yarn is not supported yet
+      npm: 'npm'
+    })
+  ]
 };
 
 export default config;
